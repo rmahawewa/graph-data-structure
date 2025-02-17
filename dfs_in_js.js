0b6534c38e5cb65c26bb9aa9545f7fa3a5e3677e
rmahawewa <rmahawewa@yahoo.com>
2025-02-17 20:52:42 +0530

    Complete traversal of disconnected undirected graph

--- dfs_in_js.js
+++ dfs_in_js.js
@@ -43,3 +43,60 @@ for(let e of edges){
 const source = 1;
 console.log("DFS from source: " + source);
 dfs(adj, source);
+
+
+
+
+//DFS for Complete Traversal of Disconnected Undirected Graph
+
+function addEdge(adj, s, t){
+    adj[s].push(t);
+    adj[t].push(s);
+}
+
+//Recursive function for DFS traversal
+function DFSRec(adj, visited, s){
+    visited[s] = true;
+    console.log(s + " ");
+
+    //Recursively visit all adjacent vertices that are not visited yet
+    adj[s].forEach(i => {
+        if(!visited[i]) {
+            DFSRec(adj, visited, i);
+        }
+    });
+}
+
+// Main DFS function to perfor, DFS for entire graph
+function DFS(adj, V){
+    let visited = new Array(V).fill(false);
+
+    //Loop through all vertices to handle disconnected graph
+    for(let i=0; i<V; i++){
+        if(!visited[i]){
+            DFSRec(adj, visited, i);
+        }
+    }
+}
+
+//Driver code
+let V1=6;
+
+//Create an adjacency list for the graph
+let adj1 = new Array(V1);
+for(let i = 0; i < V1; i++){
+    adj[i] = [];
+}
+
+//Define the edges of the graph
+let adges = [[1,2], [2,0], [0,3], [4,5] ];
+
+//Populate the adjacency list with edges
+edges.forEach(edge => { addEdge(adj, edge[0], edge[1]); });
+
+console.log("Complete DFS of the graph: ");
+dfs(adj, V);
+
+
+
+
